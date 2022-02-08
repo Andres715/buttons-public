1710de9c5bffd4bcebb2dd46b47085f958d434cc
William <49001552+Andres715@users.noreply.github.com>
2022-02-08 00:40:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ var buttonsLenght = document.getElementsByClassName("btn");
 for (var i = 0; i < buttonsLenght.length; i++) {
     buttonsLenght[i].setAttribute("title", buttonsLenght[i].textContent);
 }
-
-function cerrarModal() {
-    var modal = document.getElementsByClassName("modal");
-    modal[0].classList.add("left");
-}
-
-function downloadButtons() {
-    window.open("https://github.com/Andres715/buttons-public.git")
-}
